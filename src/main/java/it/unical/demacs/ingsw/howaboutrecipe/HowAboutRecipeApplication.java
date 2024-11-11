552e40c4626e2fbf9b15c39ae11b2d832418bd7e
gabrielegrillo <gabriele.grillo.2003@gmail.com>
2024-11-11 17:09:24 +0100

    initial commit

---
+++ src/main/java/it/unical/demacs/ingsw/howaboutrecipe/HowAboutRecipeApplication.java
@@ -0,0 +1,13 @@
+package it.unical.demacs.ingsw.howaboutrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowAboutRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowAboutRecipeApplication.class, args);
+	}
+
+}
