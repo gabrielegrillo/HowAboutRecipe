986464dbe82f8279cfc090e984c9c1c6fca658f9
BrunoCarusoc <brunocarusoc@gmail.com>
2024-12-22 10:58:33 +0100

    Progressi nella creazione della Navbar (Header)

---
+++ frontend/src/app/website/pages/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    FooterComponent,
+    HeaderComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export default class HomeComponent {
+
+}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'personal-palette-light-orange': '#FFDAB9',
+        'personal-palette-apricot': '#FBC4AB',
+        'personal-palette-melon': '#F8AD9D',
+        'personal-palette-coral-pink': '#F4978E',
+        'personal-palette-light-coral': '#F08080',
+        'personal-palette-blush': '#C86680',
+        'personal-palette-magenta-haze': '#A04B7F',
+        'personal-palette-english-violet': '#48304D',
+      }
+    },
   },
   plugins: [],
 }
