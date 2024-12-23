126462b423a5d3290d11a5bd9f08a5c1c6e5859e
BrunoCarusoc <brunocarusoc@gmail.com>
2024-12-23 14:50:11 +0100

    Modifica del path relativi ai componenti

--- frontend/src/app/business/home/home.component.ts
+++ frontend/src/app/business/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
-import { FooterComponent } from '../../../shared/components/footer/footer.component';
-import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { HeaderComponent } from '../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-home',
