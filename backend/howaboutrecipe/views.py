27f2a2e946e7e8e4592e80462dcf09d9f68ce90f
gabrielegrillo <gabriele.grillo.2003@gmail.com>
2025-01-20 00:08:58 +0100

    cleaning up

--- backend/howaboutrecipe/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Team
-
-def index(request):
-    list_teams = Team.objects.filter(team_level__exact="U09")
-    context = {'youngest_teams': list_teams}
-    return render(request, '/best/index.html', context)
